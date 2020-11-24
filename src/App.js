e69d84c75c57c413d91974eecbf42d9b2e58e326
pdlma0904 <57673815+pdlma0904@users.noreply.github.com>
2020-11-24 17:52:35 +0900

    [Create] Start Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-function App() {
-  return <div className="App"></div>;
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Main } from './Components'
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @import url(https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@1.0/nanumsquare.css);
+  *{
+    margin: 0px;
+    font-family: 'NanumSquare';
+  }
+`
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <GlobalStyle></GlobalStyle>
+      <Switch>
+        <Route exact path="/" component={Main}/>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
+
