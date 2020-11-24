64499c50373bf40a815f42a047dcdf2ede125d7a
jiyoon0605 <ljy10499@gmail.com>
2020-11-24 19:50:44 +0900

    [FIX]

--- src/component/index.js
+++ src/component/index.js
@@ -1,9 +1,11 @@
 import { Login } from "./auth/login";
 import { Signup } from "./auth/signup";
 import { Main } from "./main";
+import { Anonymous } from "./post";
 const components = {
   Login,
   Signup,
   Main,
+  Anonymous,
 };
 export default components;
