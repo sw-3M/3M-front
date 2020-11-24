6ec062997fc52e01480d704acebf833f2c911637
jiyoon0605 <ljy10499@gmail.com>
2020-11-25 00:46:51 +0900

    []

---
+++ src/component/qna/qnaContent/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 800px;
+  height: 100px;
+  background-color: #fff7e4;
+`;
+export const profile = styled.image`
+  width: 70px;
+  height: 70px;
+  border-radius: 100%;
+`;
